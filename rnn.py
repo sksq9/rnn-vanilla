be686f701fb9ebc4e533b4b8d28a2bf75ddb5cc8
Shubham Chandel <shubham.zeez@gmail.com>
2017-03-07 05:09:02 +0530

    I/O

--- rnn.py
+++ rnn.py
@@ -2,8 +2,9 @@
 # @Author: shubham
 # @Date:   2017-03-07 03:40:40
 # @Last Modified by:   shubham
-# @Last Modified time: 2017-03-07 04:50:17
+# @Last Modified time: 2017-03-07 05:08:28
 
+import sys
 import numpy as np
 
 class RNN():
@@ -16,9 +17,9 @@ class RNN():
 		self.h = np.zeros((hsize, 1))
 
 		# weights, normally distributed
-		self.Wxh = np.random.randn((insize, hsize)) * 0.01
-		self.Whh = np.random.randn((hsize, hsize)) * 0.01
-		self.Why = np.random.randn((hsize, outsize)) * 0.01
+		self.Wxh = np.random.randn(insize, hsize) * 0.01
+		self.Whh = np.random.randn(hsize, hsize) * 0.01
+		self.Why = np.random.randn(hsize, outsize) * 0.01
 
 		# biases
 		self.bh = np.zeros((hsize, 1))
@@ -49,4 +50,38 @@ class RNN():
 			loss += -p[t][targets[t]]
 
 		return loss
+
+
+def main():
+	# should be simple plain text file
+	data = open(sys.argv[1], 'r', encoding='utf-8', errors='ignore').read()
+	chars = list(set(data))
+
+	data_size, vocab_size = len(data), len(chars)
+	print('File has {} characters with {} unique.'.format(data_size, vocab_size))
+
+	# make some dictionaries for encoding and decoding from 1-of-k
+	char_to_ix = { ch:i for i,ch in enumerate(chars) }
+	ix_to_char = { i:ch for i,ch in enumerate(chars) }
+
+	# network arch for RNN
+	insize, outsize = vocab_size, vocab_size
+	hsize = 100
+	
+	rnn = RNN(insize, hsize, outsize)
+
+	# number of steps to unroll the RNN for
+	seq_length = 25 
+	
+	i, n = 0, 0		
+	while True:
+		inputs = [char_to_ix[ch] for ch in data[i:i+seq_length]]
+		targets = [char_to_ix[ch] for ch in data[i+1:i+seq_length+1]]
+
+
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt:
+		print('\nKeyboardInterrupt')
 
