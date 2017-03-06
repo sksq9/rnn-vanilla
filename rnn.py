10a75e4916d9d57dd2b4bd670e56016be316ee02
Shubham Chandel <shubham.zeez@gmail.com>
2017-03-07 04:13:24 +0530

    Class, weights init

--- rnn.py
+++ rnn.py
@@ -2,7 +2,7 @@
 # @Author: shubham
 # @Date:   2017-03-07 03:40:40
 # @Last Modified by:   shubham
-# @Last Modified time: 2017-03-07 04:08:13
+# @Last Modified time: 2017-03-07 04:13:11
 
 import numpy as np
 
@@ -13,12 +13,12 @@ class RNN():
 		self.h = np.zeros((hsize, 1))
 
 		# weights, normally distributed
-		self.Wxh = np.random.randn((hsize, insize)) * 0.01
+		self.Wxh = np.random.randn((insize, hsize)) * 0.01
 		self.Whh = np.random.randn((hsize, hsize)) * 0.01
-		self.Why = np.random.randn((outsize, insize)) * 0.01
+		self.Why = np.random.randn((hsize, outsize)) * 0.01
 
 		# biases
 		self.bh = np.zeros((hsize, 1))
 		self.by = np.zeros((outsize, 1))
 
-
+		
